ac2a7fcd01a0a57fee04538c50f9292511e81ebd
mwickramanayaka <manulwick@gmail.com>
2021-07-07 00:55:09 +0530

    screenshots added

--- src/test/java/screenshots/TakingScreenShots.java
+++ src/test/java/screenshots/TakingScreenShots.java
@@ -1,8 +1,93 @@
 package screenshots;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class TakingScreenShots {
+
+	WebDriver driver;
 	
+	@BeforeMethod
+	public void setUp() {
+		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+
+		//maximize the window 
+		driver.manage().window().maximize();
+		
+	}
 	
+
+	@Test(description="Screenshot of a complete Page")
+	public void screenShotofaPage() throws InterruptedException, IOException {
+		
+		driver.get("https://www.ebay.com");
+
+		//TakesScreenshot type object initialized
+		TakesScreenshot ts = (TakesScreenshot)driver;
+		//take the screenshot and save it as src format
+		File src = ts.getScreenshotAs(OutputType.FILE);
+		//File type object initialized
+		File target = new File("Homepage.png");
+		//copy src file to target file 
+		FileUtils.copyFile(src,target);
+
+	}
+
+	@Test(description="Screenshot of a Section In a Page")
+	public void screenShotofaSectionInaPage() throws IOException {
+
+		driver.get("https://www.ebay.com");
+		
+		WebElement pageSection = driver.findElement(By.xpath("//*[@id=\"destinations_list1\"]/ul"));
+
+		//take the screenshot and save it as src format
+		File src = pageSection.getScreenshotAs(OutputType.FILE);
+		//File type object initialized
+		File target = new File("SectionCategories.png");
+		//copy src file to target file 
+		FileUtils.copyFile(src,target);
+		
+	}
 	
+	@Test(description="Screenshot of a Section In a Page")
+	public void screenShotofaElement() throws IOException {
+
+		driver.get("https://www.ebay.com");
+		
+		WebElement ElementLogo = driver.findElement(By.xpath("//*[@id=\"gh-la\"]"));
+
+		//take the screenshot and save it as src format
+		File src = ElementLogo.getScreenshotAs(OutputType.FILE);
+		//File type object initialized
+		File target = new File("ElementLogo.png");
+		//copy src file to target file 
+		FileUtils.copyFile(src,target);
+		
+	}
+
+	@AfterMethod
+	public void tearDown() {
+
+		driver.close();
+
+	}
+
+
+
 
 }
